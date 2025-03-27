1b39331c2882cdeae7164aeb37dc5cd94f1a0c40
Amoldeep Singh <amoldeepdhillon26@gmail.com>
2025-03-27 09:34:26 +0530

    Commit Add [Amoldeep Singh] : " Commiting Generics assignment "

---
+++ Java_Generics/WarehouseManagementSystem/warehouse_items/Furniture.java
@@ -0,0 +1,8 @@
+
+package Java_Generics.WarehouseManagementSystem.warehouse_items;
+
+public class Furniture extends WarehouseItems{  // furniture
+    public Furniture(int id, String name){
+        super(id, name);
+    }
+}
